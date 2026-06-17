69a1db81252e9b4a7d978a8e2898b870e1ac5b34
qubit-go <ab.jha036@gmail.com>
2026-06-17 17:17:07 +0530

    Remove overflow-x hidden from app-container to restore default native vertical scrolling on Android Chrome, and bump version strings to v=8

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { initAudio, playNote, playChord, setMasterVolume } from './audio.js?v=7';
+import { initAudio, playNote, playChord, setMasterVolume } from './audio.js?v=8';
 import { 
   CHROMATIC_SCALE, 
   getChordVoicings, 
@@ -10,7 +10,7 @@ import {
   STRING_NAMES,
   normalizeNoteName,
   getNoteChromaticNumber
-} from './chords.js?v=7';
+} from './chords.js?v=8';
 
 // --- State Management ---
 let state = {
